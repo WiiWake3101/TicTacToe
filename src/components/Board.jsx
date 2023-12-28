36d137b0363e2e88548f2267a981a50faca05a69
WiiWake3101 <vm4512@srmist.edu.in>
2023-12-28 12:32:01 +0530

    Project Tic Tac Toe Completed Ready for deployment

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,24 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [squares, setsquares] = useState(Array(9).fill(null));
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>
   );
